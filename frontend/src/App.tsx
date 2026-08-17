7f2d5d541c0d48fcf665226317e5fe7b890c0680
Somogyi Tibor <somogyi.tibor@dataforce.hu>
2026-08-17 12:17:09 +0200

    feature/config up some base roots

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 // @ts-ignore: allow importing CSS without type declarations
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Exercises from './pages/Exercises';
+import Exercise from './pages/Exercise';
+import RootLayout from './pages/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <div>404</div>,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'exercises',
+        element: <Exercises />,
+      },
+      { path: 'exercise/:id', element: <Exercise /> },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       hello
-      </header>
+      <main className="App-main">
+        <RouterProvider router={router} />
+      </main>
     </div>
   );
 }
---
+++ frontend/src/pages/Exercises.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exercises = () => {
+  return (
+    <div>Exercise</div>
+  )
+}
+
+export default Exercises
